a226f2b643dd3c90a15f891f728c227fff70d4db
GOYOOJIN <progolovego2020@gmail.com>
2021-01-12 22:42:51 +0900

    While

--- src/step05_Array/Quiz01.java
+++ src/step05_Array/Quiz01.java
@@ -1,6 +1,37 @@
 package step05_Array;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class Quiz01 {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer X = new StringTokenizer(br.readLine());
+		int[] arr = new int[N];
 		
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(X.nextToken());
+		}
+		
+		int max = arr[0];
+		int min = arr[0];
+		
+		for(int m = 0; m<arr.length; m++) {
+			if(max<arr[m]){
+				max = arr[m];
+			}
+			if(min>arr[m]){
+				min = arr[m];
+			}
+		}
+		System.out.println(min);
+		System.out.println(max);
 	}
 }
