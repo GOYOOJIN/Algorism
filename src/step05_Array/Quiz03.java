39d07c7a1f944c3c42a25f3327cbe6f8b06d85de
GOYOOJIN <progolovego2020@gmail.com>
2021-01-14 23:12:05 +0900

    While

--- src/step05_Array/Quiz03.java
+++ src/step05_Array/Quiz03.java
@@ -9,8 +9,7 @@ import java.util.StringTokenizer;
 
 public class Quiz03 {
 	public static void main(String args[]) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
 		
 		int A = Integer.parseInt(br.readLine());
 		int B = Integer.parseInt(br.readLine());
@@ -18,13 +17,19 @@ public class Quiz03 {
 		int ABC = A*B*C;			
 		int N = (int)(Math.log10(ABC)+1);				
 		int [] arr = new int[N];		
-		
+		System.out.println(ABC);
 		for(int i = 0; i< N; i++) {
-			arr[i] = ABC%10;
+			arr[i] = ABC%10;			
+			ABC = (ABC-arr[i])/10;			
+		}	
+		int[] count = new int[10];
+		for(int j = 0; j < 10; j++) {
+			for(int a: arr) {
+				if(a == j) {
+					count[j] ++;
+				}
+			}
 		}
 		
-		
-		System.out.println(arr[1]);
-		System.out.println(ABC);		
 	}
 }
