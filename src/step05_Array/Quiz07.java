667962595a1f43bc0fdb32b574af17718d437a2d
GOYOOJIN <progolovego2020@gmail.com>
2021-01-27 23:50:36 +0900

    Array

--- src/step05_Array/Quiz07.java
+++ src/step05_Array/Quiz07.java
@@ -4,25 +4,37 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Quiz07 {
 	public static void main(String args[]) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int C = Integer.parseInt(br.readLine());
-		int i;
-		int j;
+		Scanner scan = new Scanner(System.in);
 		
-		String score = "";	
-
-		for (i = 0; i < C; i++) {
-			score = br.readLine();	
-			String[] scoreArr = new String[C];	
-			scoreArr[i] = score;
-			for(j = 0; j < score.length(); j++) {
-				scoreArr[i].split("");
+		int[] arr;		
+		int testcase = scan.nextInt();
+		
+		for(int i = 0 ; i < testcase ; i++) { 
+			int N = scan.nextInt();	
+			arr = new int[N];			
+			double sum = 0;				
+			for(int j = 0 ; j < N ; j++) {
+				int val = scan.nextInt();	
+				arr[j] = val;
+				sum += val;
 			}
-		}		
-
+			
+			double mean = (sum / N) ;
+			double count = 0; 			
+			
+			for(int j = 0 ; j < N ; j++) {
+				if(arr[j] > mean) {
+					count++;
+				}
+			}
+			
+			System.out.printf("%.3f%%\n",(count/N)*100);				
+		}
+		scan.close();
 	}
 }
